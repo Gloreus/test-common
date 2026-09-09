b8624bcf04751ae392312a367c2748f657b838ac
start.vaadin.com <no-reply@vaadin.com>
2026-09-09 10:54:11 +0000

    Generated project

---
+++ src/main/java/ru/sqbt/plaqltests/base/ui/package-info.java
@@ -0,0 +1,4 @@
+@NullMarked
+package ru.sqbt.plaqltests.base.ui;
+
+import org.jspecify.annotations.NullMarked;
---
+++ src/main/java/ru/sqbt/plaqltests/examplefeature/package-info.java
@@ -0,0 +1,5 @@
+@NullMarked
+package ru.sqbt.plaqltests.examplefeature;
+// TODO Remove this package once you have added real features
+
+import org.jspecify.annotations.NullMarked;
